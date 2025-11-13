1fa1f2a5bbe1f624bc98a7a40d3f23a303a583b3
OmarMesqq <omarmsqt@gmail.com>
2025-11-13 14:35:52 -0300

    refactor: :recycle: move lines <= MAX_LINES guard to inside loop

--- sbas.c
+++ sbas.c
@@ -119,13 +119,21 @@ funcp sbasCompile(FILE* f) {
   /**
    * First pass: emit most instructions and leave 4-byte placeholders for jumps
    */
-  while (fgets(lineBuffer, sizeof(lineBuffer), f) && line <= MAX_LINES) {
+  while (fgets(lineBuffer, sizeof(lineBuffer), f)) {
     printf("lineBuffer: %s\n", lineBuffer);
     printf("lineBuffer[0]: %c (str), %d (dec), %02x (hex)\n", lineBuffer[0], lineBuffer[0], lineBuffer[0]);
     // Skip comment lines, but still increase the parsed lines counter
     if (lineBuffer[0] == '/') {
       line++;
       continue;
+    }
+
+    if (line > MAX_LINES) {
+      fprintf(stderr, "sbasCompile: the provided SBas file exceeds MAX_LINES (%d)!\n", MAX_LINES);
+      free(lt);
+      free(rt);
+      sbasCleanup((funcp) code);
+      return NULL;
     }
     
     lt[line].line = line;
